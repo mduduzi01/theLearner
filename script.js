510380b643f9b36377ee1efb0151a94400e7615d
unknown <mokgosinyanamduduzi@gmail.com>
2021-11-09 07:54:40 +0200

    auto

---
+++ script.js
@@ -0,0 +1,9 @@
+//const Learners = ["Mduduzi","Kagisho","Lucky","Sophie","Precious","Selina","Happy","Andisiwe","Khumbulani","Mmapaseka","Sithembiso","Ntombela","Wandile","Vusimuzi","Nkosiphendule"];
+
+//const auto = document.getElementById("auto");
+
+//for(let a = 0; a <Learners.length; a++){
+
+     //   auto.innerHTML += "<li>" + Learners [a]
+
+//}
