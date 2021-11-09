c92b3a34b69afc71b35d01b48d1d649f0bff7df6
unknown <mokgosinyanamduduzi@gmail.com>
2021-11-09 14:35:45 +0200

    student added

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for(let a = 0; a <Learners.length; a++){
         auto.innerHTML += "<hr>" + Learners [a] + "</hr>";
 
 }
+
+var student = ["Mduduzi","Kagisho","Lucky","Sophie","Precious","Selina","Happy","Andisiwe","Khumbulani","Mmapaseka","Sithembiso","Ntombela","Wandile","Vusimuzi","Nkosiphendule"];
+
+var text = document.getElementById("text");
+
+for(let a = 0; a <Learners.length; a++){
+
+        text.innerHTML += "<li>" + student [a] + "</li>";
+
+}
